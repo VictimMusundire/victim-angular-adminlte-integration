65e340f6ffc423327f6e5561be95f161ab6e215e
Victim Musundire <vmusundire@bancabc.com>
2021-05-13 12:22:53 +0200

    refactored the ts for all forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { ApplicationslistComponent } from './pages/applicationslist/applicationslist.component';
 import { ApplyformComponent } from './pages/applyform/applyform.component';
 import { ExtractfilesComponent } from './pages/extractfiles/extractfiles.component';
@@ -18,11 +14,7 @@ import { ExtractfilesComponent } from './pages/extractfiles/extractfiles.compone
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent,
     HomeComponent,
-    AboutComponent,
-    SidebarComponent,
-    FooterComponent,
     ApplicationslistComponent,
     ApplyformComponent,
     ExtractfilesComponent
