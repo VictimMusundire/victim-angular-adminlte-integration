04d0cce38d06739685a2ca94f547acbc66e58b9d
Victim Musundire <vmusundire@bancabc.com>
2021-06-03 11:16:18 +0200

    edited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
-import {LoginComponent} from './pages/login/login.component';
 import {ApplicationslistComponent} from './pages/applicationslist/applicationslist.component';
 import {ApplyformComponent} from './pages/applyform/applyform.component';
 import {PagenotfoundComponent} from './pages/pagenotfound/pagenotfound.component';
@@ -12,8 +11,7 @@ const appRoutes: Routes = [
       {path: 'home', component: HomeComponent},
       {path: 'applications', component: ApplicationslistComponent},
       {path: 'apply', component: ApplyformComponent},
-      {path: 'login', component: LoginComponent},
-      // {path: '**', component : PagenotfoundComponent}
+      {path: '**', component : PagenotfoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ApplicationslistComponent } from './pages/applicationslist/applicationslist.component';
 import { ApplyformComponent } from './pages/applyform/applyform.component';
-import { ExtractfilesComponent } from './pages/extractfiles/extractfiles.component';
 import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HomeComponent,
     ApplicationslistComponent,
     ApplyformComponent,
-    ExtractfilesComponent,
     PagenotfoundComponent
   ],
   imports: [
